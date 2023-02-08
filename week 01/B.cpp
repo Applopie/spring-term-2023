dce21b8704bb7da4b0782268fdcef520c67c3bb7
Applopie <117065570+Applopie@users.noreply.github.com>
2023-02-08 21:31:35 +0300

    Update B.cpp

--- week 01/B.cpp
+++ week 01/B.cpp
@@ -7,7 +7,13 @@ int main() {
     int a = 1, b = 2, c = 1;
     cin >> a >> b >> c;
     if (b*b - 4*a*c > 0) {
-        cout << ((-b + sqrt(b*b - 4*a*c))/(2*a)) << " "<< ((-b - sqrt(b*b - 4*a*c))/(2*a)) << endl;
+        float x1 = ((-b - sqrt(b*b - 4*a*c))/(2*a)), x2 = ((-b + sqrt(b*b - 4*a*c))/(2*a));
+        if (x1 > x2) {
+            cout << x2 << " "<< x1 << endl;
+        }
+        else {
+            cout << x1 << " " << x2 << endl;
+        }
     }
     else if (b*b - 4*a*c == 0) {
         cout << ((-b + sqrt(b*b - 4*a*c))/(2*a)) << endl;

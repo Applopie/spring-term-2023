38052c012baf873979fed9ef0eb8868b6df5bc28
Applopie <stina.nekrasova.2@yandex.ru>
2023-02-19 20:38:44 +0300

    new things so

---
+++ week03/D.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#ifndef N
+#define N 5
+#endif
+
+void swap(int& lha, int& rha) {
+    int h = lha;
+    lha = rha;
+    rha = h;
+}
+
+bool comp(int& a, int& b) {
+    return a >= b;
+}
+
+int main() {
+    int a[N];
+    int k = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+    sort(a, a + N, comp);
+    for (int j = 0; j < N; j++) {
+        if (a[j] >= 0) {
+            k++;
+        }
+    }
+    sort(a, a + k);
+    for (int i = 0; i < N; i++) {
+        cout << a[i] << ' ';
+    }
+    return 0;
+}

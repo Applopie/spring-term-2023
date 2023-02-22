9561595c1caecae303db11e0992b770a14f5fca5
Applopie <stina.nekrasova.2@yandex.ru>
2023-02-22 15:33:04 +0300

    lab and also e?

---
+++ lab1/search1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <cmath>
+
+#ifndef N
+#define N 100
+#endif
+
+using namespace std;
+
+int func(int (&array)[N], int a) {
+    for (int i = 0; i < N; i++) {
+        if (array[i] == a) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int a[N];
+
+    auto begin = std::chrono::steady_clock::now();
+    for (unsigned cnt = 100000; cnt != 0; --cnt) 
+    func();
+    auto end = std::chrono::steady_clock::now();
+    auto time_span = std::chrono::duration_cast<std::chrono::milliseconds>(end-begin);
+    cout << "\n\n";
+    cout << time_span.count() << endl;
+    return 0;
+}

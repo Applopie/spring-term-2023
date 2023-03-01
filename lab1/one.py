3aa3e1cf71add447a61f156b8bd46a02b3f8d6b7
Applopie <117065570+Applopie@users.noreply.github.com>
2023-03-01 21:45:01 +0300

    Create one.py

---
+++ lab1/one.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = [100, 200, 500, 700, 1000, 2000, 5000, 7000, 10000, 20000, 50000, 70000, 100000, 200000, 500000]
+y = [34, 63, 177, 187, 402, 740, 1255, 1743, 3456, 6740, 11879, 16839, 27318, 55519, 129116]
+
+plt.xticks(np.arange(0, 600000, step = 100000))
+plt.yticks(np.arange(0, 1500000, step = 100000))
+
+plt.scatter(x, y, s=20, c='b')
+plt.plot(x,y)
+
+plt.xlabel('Array size')
+plt.ylabel('Time, microseconds')
+plt.show()

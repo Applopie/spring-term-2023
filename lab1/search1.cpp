8a5aea23537de8997672e704f2f723fe71c4ffb0
Applopie <stina.nekrasova.2@yandex.ru>
2023-03-02 10:32:29 +0300

    auiu

--- lab1/search1.cpp
+++ lab1/search1.cpp
@@ -5,12 +5,12 @@
 #include <algorithm>
 
 #ifndef N
-#define N 1000000
+#define N 300000
 #endif
 
 using namespace std;
 
-int func(int (&array)[N], int a) {
+int func(int (array)[N], int a) {
     for (int i = 0; i < N; i++) {
         if (array[i] == a) {
             return i;
@@ -19,22 +19,29 @@ int func(int (&array)[N], int a) {
     return -1;
 }
 
-int funcbin(int (&array)[N], int a) {
-    int l = 0, r = N - 1, it = 0;
-    sort(array, array + N);
-    while (l < r) {
-        it = (l + r)/2;
-        if (array[it] == a) {
-            return it;
-        }
-        else if (array[it] > a) {
-            r = it - 1;
-        }
-        else {
-            l = it + 1;
-        }
+int funcbin(int (&array)[N], int l, int r, int a) {
+    int it = (l + r) / 2.;
+    if (array[l] == a) {
+        return array[l];
+    } 
+    else if (array[r] == a) {
+        return array[r];
+    } 
+    else if (array[l + 1] == array[r]){
+        return -1;
+    } 
+    else if (array[r] < a) {
+        return -1;
     }
-    return -1;
+    else if (array[l] > a) {
+        return -1;
+    } 
+    else if (array[it] < a) {
+        return funcbin(array, it, r, a);
+    }
+     else if (array[it] > a) {
+        return  funcbin(array, l, it, a);
+    }
 }
 
 int main() {
@@ -47,12 +54,13 @@ int main() {
         a[i] = dstr(rng);
     }
     int t = 0, tt = 0;
-    //int b = dstr(rng);
-    int b = -100;
-    for (int j = 40; j > 0; j--) {
+    int b = dstr(rng);
+    //int b = -100;
+    sort(a, a + N);
+    for (int j = 100; j > 0; j--) {
     auto begin = std::chrono::steady_clock::now();
-    for (unsigned cnt = 100000; cnt != 0; --cnt) 
-    func(a, b);
+    for (unsigned cnt = 1000000; cnt != 0; --cnt) 
+    funcbin(a, 0, N - 1, b);
     auto end = std::chrono::steady_clock::now();
     auto time_span = std::chrono::duration_cast<std::chrono::milliseconds>(end-begin);
     cout << "\n\n";
--- lab1/searchsum1.cpp
+++ lab1/searchsum1.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 
 #ifndef N
-#define N 100
+#define N 5100
 #endif
 
 using namespace std;
@@ -20,29 +20,45 @@ int func(int (&array)[N], int a) {
     return -1;
 }
 
-int main() {
-    int a[N];
-    unsigned seed = 1001;
-    std::default_random_engine rng(seed);
-    std::uniform_int_distribution<unsigned>dstr(0, N);
+int funcbin(int array[N], int a) {
+    int l = 0, r = N-1;
+    while ((l < N - 1) and (r > 0)) {
+        if (array[l] + array[r] == a) {
+            return l, r;
+        } 
+        else if (array[l] + array[r] > a) {
+            r--;
+        } 
+        else {
+            l++;
+        }
+    }      
+    return -1;
+}
 
-    for (int i = 0; i < N; i++) {
-        a[i] = dstr(rng);
-    }
-    int t = 0, tt = 0;
-    //int b = dstr(rng);
-    int b = -100;
-    for (int j = 40; j > 0; j--) {
-    auto begin = std::chrono::steady_clock::now();
-    for (unsigned cnt = 100000; cnt != 0; --cnt) 
-    func(a, b);
-    auto end = std::chrono::steady_clock::now();
-    auto time_span = std::chrono::duration_cast<std::chrono::milliseconds>(end-begin);
-    cout << "\n\n";
-    cout << time_span.count() << endl;
-    t ++;
-    tt = tt + time_span.count();
-    }
-    cout << tt/t << endl;
-    return 0;
+int main() {
+        int a[N];
+        unsigned seed = 1001;
+        std::default_random_engine rng(seed);
+        std::uniform_int_distribution<unsigned>dstr(0, N);
+
+        for (int i = 0; i < N; i++) {
+            a[i] = dstr(rng);
+        }
+        int t = 0, tt = 0;
+        int b = a[10]  + a[35];
+        //int b = -100;
+        for (int j = 100; j > 0; j--) {
+        auto begin = std::chrono::steady_clock::now();
+        for (unsigned cnt = 100000; cnt != 0; --cnt) 
+        funcbin(a, b);
+        auto end = std::chrono::steady_clock::now();
+        auto time_span = std::chrono::duration_cast<std::chrono::milliseconds>(end-begin);
+        cout << "\n\n";
+        cout << time_span.count() << endl;
+        t ++;
+        tt = tt + time_span.count();
+        }
+        cout << tt/t << endl;
+        return 0;
 }

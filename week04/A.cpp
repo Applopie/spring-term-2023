655e651887829f628a20726081f717ca2948ee89
Applopie <stina.nekrasova.2@yandex.ru>
2023-03-02 12:43:03 +0300

    svt right here

---
+++ week04/A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef N
+#define N 10
+#endif
+
+void swap(int a, int b) {
+    
+}
+
+int main() {
+    int a[N];
+    for (i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+    for (i = 1; i < N; i++) {
+        if 
+    }
+}

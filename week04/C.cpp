e7a4840059fbe4af062db6ec59ecc07a5ea4e6da
Applopie <stina.nekrasova.2@yandex.ru>
2023-03-03 22:08:50 +0300

    fuck it

--- week04/C.cpp
+++ week04/C.cpp
@@ -8,32 +8,23 @@ using namespace std;
 #endif
 
 void swapp(int (&arr)[], int n, int i) {
-    int hd = i;
-    int l = 2*i + 1;
-    int r = 2*i + 2;
-    if (l < n && arr[l] > arr[hd]) {
-        hd = l;
+    if (2*n + 1 <= i & arr[n] < arr[2*n + 1]) {
+        swap(arr[n], arr[2*n + 1]);
     }
-    if (r < n && arr[r] > arr[hd]) {
-        hd = r;
-    }
-    if (hd != i) {
-        swap(arr[i], arr[hd]);
-        swapp(arr, n, hd);
-    }
-    if (l < n && r < n && arr[l] < arr[r]) {
-        swap(arr[r], arr[l]);
+    if (2*n + 2 <= i && arr[n] < arr[2*n + 2]) {
+        swap(arr[n], arr[2*n + 2]);
     }
 }
 
-void hs(int (&arr)[], int n) {
-    for (int i = n/2 - 1; i > 0; i--) {
-        swapp(arr, n, i);
+void hs(int (&arr)[N], int in, int sz) {
+    if (in = 0) {
+        return;
     }
-    for (int i = N - 1; i > 0; i--) {
-        swap(arr[i], arr[0]);
-        swapp(arr, n, 0);
+    for (int i = in; i >= 0; i--) {
+        swapp(arr, i, in);
     }
+    swap(arr[0], arr[in]);
+    hs(arr, in - 1, sz);
 }
 
 int main() {
@@ -41,8 +32,8 @@ int main() {
     for (int i = 0; i < N; i++) {
         cin >> a[i];
     }
-    hs(a, N);
-    for (int i = N - 1; i >= 0; i--) {
+    hs(a, N - 1, N);
+    for (int i = 0; i < N; i++) {
         cout << a[i] << ' ';
     }
 }

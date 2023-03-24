4c4930c49c12a39d7b1e9db35fd5e51fb80e5cf6
Applopie <stina.nekrasova.2@yandex.ru>
2023-03-24 12:10:59 +0300

    lachimolala

--- week06/F.cpp
+++ week06/F.cpp
@@ -1,1 +1,18 @@
 // and i oops
+#include <iostream>
+
+using namespace std;
+
+enum
+
+    int
+    main()
+{
+    int error = 1;
+    while (error != 0)
+    {
+        cin >> error;
+        if ()
+    }
+    return 0;
+}

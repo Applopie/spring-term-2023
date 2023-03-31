c84dc176887a9cbed96c5ac54b5a4a773c6ee68e
Applopie <stina.nekrasova.2@yandex.ru>
2023-03-31 11:57:14 +0300

    my life sucks so much that i wanna be yamaguchi not bad but good for me

--- week 07/linked_list.cpp
+++ week 07/linked_list.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 struct Node
 {
     int key = 0;
@@ -37,14 +39,94 @@ void push_back(Node *&head_ref, int new_key)
     return;
 }
 
-void push_back(Node *&prev_ref, int new_key)
+void insert_after(Node *&prev_ref, int new_key)
 {
     if (prev_ref == nullptr)
     {
         return;
     }
     Node *el = prev_ref->next;
-    el->next = new Node;
-    el->next->key = new_key;
+    prev_ref->next = new Node;
+    prev_ref->next->key = new_key;
+    prev_ref->next->next = el;
     return;
+}
+
+void destroy_list(Node *&head_ref)
+{
+    if (head_ref == nullptr)
+    {
+        return;
+    }
+    while (head_ref->next != nullptr)
+    {
+        Node *el = head_ref->next->next;
+        delete head_ref->next;
+        head_ref->next = el;
+    }
+    delete head_ref->next;
+    head_ref = nullptr;
+    return;
+}
+
+void print_list(Node *&head_ref)
+{
+    if (head_ref == nullptr)
+    {
+        std::cout << '\n';
+        return;
+    }
+    Node *el = head_ref;
+    while (el->next != nullptr)
+    {
+        std::cout << el->key << ' ';
+        el = el->next;
+    }
+    std::cout << el->key << std::endl;
+    return;
+}
+
+Node *find(Node *&head_ref, int key)
+{
+    if (head_ref == nullptr)
+    {
+        return nullptr;
+    }
+    Node *el = head_ref;
+    while (el != nullptr)
+    {
+        if (el->key == key)
+        {
+            return el;
+        }
+    }
+    return nullptr;
+}
+
+void *find(Node *&head_ref, int key)
+{
+    if (head_ref == nullptr)
+    {
+        return;
+    }
+    if (head_ref->key == key)
+    {
+        Node *el = head_ref->next;
+        delete head_ref;
+        head_ref = el;
+        return;
+    }
+    Node *el = head_ref;
+    while (el->next != nullptr)
+    {
+        if (el->next->key == key)
+        {
+            Node *el2 = el->next->next;
+            delete el->next;
+            el->next = el2;
+            return;
+        }
+        el = el->next;
+        return;
+    }
 }

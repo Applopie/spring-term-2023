78bdcdbb23aaacf411d500a72b045502127d48ed
Applopie <stina.nekrasova.2@yandex.ru>
2023-04-05 18:15:09 +0300

    and i never ever ever get my shit together

--- week 07/linked_list2.cpp
+++ week 07/linked_list2.cpp
@@ -25,3 +25,84 @@ List *create_empty_list()
     List->TAIL = List->NIL;
     return List;
 }
+
+List *push_front(List *list_ptr, int new_key)
+{
+    if (list_ptr == nullptr)
+    {
+        return nullptr;
+    }
+    if (list_ptr->HEAD == list_ptr->NIL)
+    {
+        list_ptr->HEAD = new Node;
+        list_ptr->HEAD->key = new_key;
+        list_ptr->HEAD->prev = list_ptr->NIL;
+        list_ptr->HEAD->next = list_ptr->NIL;
+        list_ptr->TAIL = list_ptr->HEAD;
+        list_ptr->size++;
+        return list_ptr;
+    }
+    if (list_ptr->HEAD->next == list_ptr->NIL)
+    {
+        list_ptr->HEAD = new Node;
+        list_ptr->HEAD->key = new_key;
+        list_ptr->HEAD->prev = list_ptr->NIL;
+        list_ptr->HEAD->next = list_ptr->TAIL;
+        list_ptr->TAIL->prev = list_ptr->HEAD;
+        list_ptr->size++;
+        return list_ptr;
+    }
+    Node *el = list_ptr->HEAD;
+    list_ptr->HEAD = new Node;
+    list_ptr->HEAD->key = new_key;
+    list_ptr->HEAD->prev = list_ptr->NIL;
+    list_ptr->HEAD->next = el;
+    el->prev = list_ptr->HEAD;
+    list_ptr->size++;
+    return list_ptr;
+}
+
+List *push_back(List *list_ptr, int new_key)
+{
+    if (list_ptr == nullptr)
+    {
+        return nullptr;
+    }
+    if (list_ptr->HEAD == list_ptr->NIL)
+    {
+        list_ptr->TAIL = new Node;
+        list_ptr->TAIL->key = new_key;
+        list_ptr->TAIL->prev = list_ptr->NIL;
+        list_ptr->TAIL->next = list_ptr->NIL;
+        list_ptr->TAIL = list_ptr->HEAD;
+        list_ptr->size++;
+        return list_ptr;
+    }
+    if (list_ptr->HEAD->next == list_ptr->NIL)
+    {
+        list_ptr->TAIL = new Node;
+        list_ptr->TAIL->key = new_key;
+        list_ptr->TAIL->prev = list_ptr->HEAD;
+        list_ptr->TAIL->next = list_ptr->NIL;
+        list_ptr->HEAD->next = list_ptr->TAIL;
+        list_ptr->size++;
+        return list_ptr;
+    }
+    Node *el = list_ptr->TAIL;
+    list_ptr->TAIL = new Node;
+    list_ptr->TAIL->key = new_key;
+    list_ptr->TAIL->next = list_ptr->NIL;
+    list_ptr->TAIL->prev = el;
+    el->next = list_ptr->TAIL;
+    list_ptr->size++;
+    return list_ptr;
+}
+
+List *insert_after(List *list_ptr, Node *prev_ptr, int new_key)
+{
+    if (list_ptr == nullptr)
+    {
+        return nullptr;
+    }
+    if
+}

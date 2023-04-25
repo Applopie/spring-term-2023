823f84f4dfa0cd61483407f4bfd6d46e483aef2a
Applopie <stina.nekrasova.2@yandex.ru>
2023-04-25 14:11:48 +0300

    you know what fuck you and so on

--- week10/E2.cpp
+++ week10/E2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,22 +11,40 @@ int main()
     std::cin >> s1 >> s2;
     int a1 = s1.length();
     int a2 = s2.length();
-    if (a1 < a2)
+    if (s1.length() < s2.length())
     {
-        int a3 = a1;
-        a1 = a2;
-        a2 = a3;
+        swap(s1, s2);
     }
-    int a[a1];
-    int lenght = 0;
-    int t = 0, j = 0;
-    while (j < a2)
+    int zero[1000];
+    int one[1000];
+    for (int i = 0; i < a2; i++)
     {
-        while (t != 1)
+        zero[i] = 0;
+        one[i] = 0;
+    }
+    for (int i = 1; i <= a1; i++)
+    {
+        one[0] = 0;
+        for (int j = 1; j <= a2; j++)
         {
-            if (a)
+            zero[j] = one[j];
+            if (s1[i] == s2[j])
+            {
+                one[j] = zero[j - 1] + 1;
+            }
+            else
+            {
+                if (zero[j] >= one[j - 1])
+                {
+                    one[j] = zero[j];
+                }
+                else if (zero[j] < one[j - 1])
+                {
+                    one[j] = one[j - 1];
+                }
+            }
         }
     }
-    cout << lenght << endl;
+    cout << (one[a2] / s2.length()) * 100 << endl;
     return 0;
 }

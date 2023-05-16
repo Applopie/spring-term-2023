d09f8af72ef6ef67522cf3940df6c73ee73b9681
Applopie <stina.nekrasova.2@yandex.ru>
2023-05-16 22:54:47 +0300

    aaaAAAaaaAAAASAaaa

--- test/swaps.cpp
+++ test/swaps.cpp
@@ -9,19 +9,20 @@ void swap1(int &a, int &b)
     b = t;
 }
 
-void swap2(int **a, int **b)
+void swap2(int *a, int *b)
 {
-    int *p = *a;
+    int *p = new int;
+    *p = *a;
     *a = *b;
-    *b = p;
+    *b = *p;
+    delete p;
 }
 
 int main()
 {
     int a = 6, b = 9;
     int *a1 = &a, *b1 = &b;
-    int **a2 = &a1, **b2 = &b1;
-    swap2(a2, b2);
-    cout << a << ' ' << b << endl;
+    swap2(a1, b1);
+    cout << *a1 << ' ' << *b1 << endl;
     return 0;
 }
